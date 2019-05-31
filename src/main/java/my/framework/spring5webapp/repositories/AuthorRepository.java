58cb1cb076bcc1a32da0ceb5f492ad084cd951d4
bandyopadhyay <46854064+bandyopadhyay@users.noreply.github.com>
2019-05-30 20:24:49 -0500

    Update AuthorRepository.java

--- src/main/java/my/framework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/my/framework/spring5webapp/repositories/AuthorRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import my.framework.spring5webapp.model.Author;
 
-/**
- * Created by bandyopadh on 2/25/19.
- */
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
