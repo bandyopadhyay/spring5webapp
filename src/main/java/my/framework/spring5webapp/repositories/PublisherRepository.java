e557fb66f25e7105bc04765410b3894858137b55
bandyopadhyay <46854064+bandyopadhyay@users.noreply.github.com>
2019-05-30 20:25:09 -0500

    Update PublisherRepository.java

--- src/main/java/my/framework/spring5webapp/repositories/PublisherRepository.java
+++ src/main/java/my/framework/spring5webapp/repositories/PublisherRepository.java
@@ -5,8 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import my.framework.spring5webapp.model.Book;
 import my.framework.spring5webapp.model.Publisher;
 
-/**
- * Created by bandyopadh on 2/25/19.
- */
 public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
